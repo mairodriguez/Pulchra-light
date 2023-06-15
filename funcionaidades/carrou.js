5333e12e0446798060ba47c058d844faa95caa81
mai <mailyyuliana.rodriguez@estudioenpenascal.com>
2023-06-15 16:29:41 +0200

    cambios dia 2

---
+++ funcionaidades/carrou.js
@@ -0,0 +1,6 @@
+const carru = document.querySelector(".imagen");
+const adelante = document.querySelector(".adelante");
+
+adelante.addEventListener("click", ()=> {
+    carru.src ="imagenes/fondoimg.png";
+})
