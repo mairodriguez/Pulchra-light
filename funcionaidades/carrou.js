84ab82718b196f7203e80dfa15ae464cd00d0206
mai <mailyyuliana.rodriguez@estudioenpenascal.com>
2023-06-15 16:36:59 +0200

    carrousel

--- funcionaidades/carrou.js
+++ funcionaidades/carrou.js
@@ -1,6 +1,28 @@
 const carru = document.querySelector(".imagen");
 const adelante = document.querySelector(".adelante");
-
+ let counter = 1 ;
 adelante.addEventListener("click", ()=> {
-    carru.src ="imagenes/fondoimg.png";
+
+    
+    
+    if(counter == 1){
+        carru.src = "imagenes/bandera" + counter + "png";
+        counter++;
+    }
+    else if(counter == 2){
+        carru.src = "imagenes/bandera" + counter + "png";
+        counter++;
+    }
+    else if(counter == 3){
+        carru.src = "imagenes/bandera" + counter + "png";
+        counter++;
+    }
+    else if(counter == 4){
+        carru.src = "imagenes/bandera" + counter + "png";
+        counter++;
+    }
+    else if(counter == 5){
+        carru.src = "imagenes/bandera" + counter + "png";
+        counter= 1;
+    }
 })
